983bcf80fd2917bdedd263ff431406c33a64ee96
Vaimer9 <vaim3r9@gmail.com>
2021-11-19 14:34:31 +0530

    halfway commit

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,18 @@ struct Parsed_json {
     
 }
 
+fn get_data(file: &str) -> Result<String, std::io::Error> {
+    let _ = fs::read_to_string(filename)?;
 
-fn main() {
+    return _;
+}
+
+fn main() -> Result<(), std::io::Error> {
+    let data = get_data("data.json")?;
+
+    let json_data: serde_json::Value = serde_json::from_str(data)?;
+
     println!("Hello, world!");
+
+    Ok(())
 }
